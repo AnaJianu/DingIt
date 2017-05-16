c36b3371af3c2ecb38879554c324fb9064fe5d62
Ana Jianu <ana.jianu@yahoo.com>
2017-05-17 00:41:15 +0300

    first commit

---
+++ src/main/java/ro/anajianu/dingit/DingitApplication.java
@@ -0,0 +1,12 @@
+package ro.anajianu.dingit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DingitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DingitApplication.class, args);
+	}
+}
