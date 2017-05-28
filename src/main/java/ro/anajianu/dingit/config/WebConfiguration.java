866449d941fb9baf1a03cefdc08d906f48c491cb
Ana Jianu <ana.jianu@yahoo.com>
2017-05-28 20:35:21 +0300

    Added basic implementation for i18n.
    After adding messages.properties, the app configuration needed the annotation @EnableWebMvc to allow customized messages loading.

--- src/main/java/ro/anajianu/dingit/config/WebConfiguration.java
+++ src/main/java/ro/anajianu/dingit/config/WebConfiguration.java
@@ -1,10 +1,17 @@
 package ro.anajianu.dingit.config;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
@@ -15,9 +22,12 @@ import org.thymeleaf.templateresolver.UrlTemplateResolver;
  * Created by ana on 28/05/2017.
  */
 @Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "ro.anajianu.dingit")
 public class WebConfiguration extends WebMvcConfigurationSupport {
 
     private static final String VIEWS_PREFIX = "/WEB-INF/views/";
+    private static final String MESSAGE_SOURCE ="/WEB-INF/i18n/messages";
     private static final String RESOURCES_LOCATION = "/resources/";
     private static final String RESOURCES_HANDLER = RESOURCES_LOCATION + "**";
 
@@ -48,8 +58,30 @@ public class WebConfiguration extends WebMvcConfigurationSupport {
         return  thymeleafViewResolver;
     }
 
+    @Bean(name = "messageSource")
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename(MESSAGE_SOURCE);
+        messageSource.setCacheSeconds(5);
+        return messageSource;
+    }
+
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler(RESOURCES_HANDLER).addResourceLocations(RESOURCES_LOCATION);
+    }
+
+    @Override
+    protected void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public RequestMappingHandlerMapping requestMappingHandlerMapping() {
+        RequestMappingHandlerMapping requestMappingHandlerMapping = super.requestMappingHandlerMapping();
+        requestMappingHandlerMapping.setUseSuffixPatternMatch(false);
+        requestMappingHandlerMapping.setUseTrailingSlashMatch(false);
+        return requestMappingHandlerMapping;
+
     }
 }
