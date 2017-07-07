d2c2235ed4c298510fd8bcd077b76d92d8cf525a
Ana Jianu <ana.jianu@yahoo.com>
2017-07-07 09:02:48 +0300

    added basic implementation for retrieving user questions

--- src/main/java/ro/anajianu/dingit/controller/HomepageController.java
+++ src/main/java/ro/anajianu/dingit/controller/HomepageController.java
@@ -1,12 +1,17 @@
 package ro.anajianu.dingit.controller;
 
+import org.hibernate.ObjectDeletedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import ro.anajianu.dingit.model.User;
 import ro.anajianu.dingit.repository.UserRepository;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by ana on 25/06/2017.
@@ -23,6 +28,13 @@ public class HomepageController {
     @RequestMapping(value = "/")
     public String showHomepage() {
         return "homepage/homepage";
+    }
+
+    @RequestMapping(value = "/user/questions")
+    @ResponseBody
+    public Map<String, Object> getAllQuestionsForCurrentUser () {
+        Map<String, Object> result = new HashMap<>();
+        return result;
     }
 
 }
